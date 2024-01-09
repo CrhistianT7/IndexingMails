d67d1673c88e3c5f04dee4c68f67de9505fff276
Crhistian Turpo <andrewturpo573@gmail.com>
2024-01-08 22:53:35 -0500

    web server started

--- backend-indexing/cmd/api/main.go
+++ backend-indexing/cmd/api/main.go
@@ -1,7 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const port = 8080
+
+type application struct {
+	Domain string
+}
 
 func main() {
-	fmt.Println("hello from backend")
+	// args := os.Args
+	// fmt.Println("hello from backend")
+	// fmt.Println(args[1])
+	// _, err := os.ReadDir("../../" + args[1] + "/maildir")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// -------------------------------------------
+	// HERE IT BEGINS
+	// set application config
+	var app application
+	// read from command line
+
+	// connect to the database
+	app.Domain = "example.com"
+	log.Println("Starting application on port", port)
+	// start a web server
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// -------------------------------------------
+
+	// fmt.Println(entries)
+
+	// for _, e := range entries {
+	// 	fmt.Println(e.Name())
+	// 	fmt.Println("----")
+	// 	ent, err := os.ReadDir("../../" + args[1] + "/maildir/" + e.Name())
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// 	for _, f := range ent {
+	// 		fmt.Println(f.Name())
+
+	// 		dat, err := os.ReadFile("../../enron_mail_20110402/maildir/" + e.Name() + "/" + f.Name() + "/1.")
+	// 		if err != nil {
+
+	// 		}
+	// 		fmt.Println("\n begin *****")
+	// 		fmt.Print(string(dat))
+	// 		fmt.Println("\n end *****")
+	// 	}
+	// }
 }
