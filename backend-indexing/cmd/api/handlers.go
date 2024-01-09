5c6ed2f75a497fbf988cf110447b5334f1947cec
Crhistian Turpo <andrewturpo573@gmail.com>
2024-01-08 23:07:20 -0500

    home router added

--- backend-indexing/cmd/api/handlers.go
+++ backend-indexing/cmd/api/handlers.go
@@ -1,10 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"log"
 	"net/http"
 )
 
-func Hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello world")
+func (app *application) Home(w http.ResponseWriter, r *http.Request) {
+	var payload = struct {
+		Status  string `json:"status"`
+		Message string `jason:"message"`
+		Version string `json:"version"`
+	}{
+		Status:  "active",
+		Message: "Indexing backend up and running",
+		Version: "1.0.0",
+	}
+
+	out, err := json.Marshal(payload)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(out)
 }
--- backend-indexing/go.mod
+++ backend-indexing/go.mod
@@ -2,4 +2,4 @@ module backend
 
 go 1.21.1
 
-require github.com/go-chi/chi/v5 v5.0.11 // indirect
+require github.com/go-chi/chi/v5 v5.0.11
