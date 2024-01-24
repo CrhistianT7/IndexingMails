e6f170534e452e55513088b569c6a94baf23e816
Crhistian Turpo <andrewturpo573@gmail.com>
2024-01-24 11:34:50 -0500

    benchmarkCorrection

--- backend-indexing/cmd/api/index_test.go
+++ backend-indexing/cmd/api/index_test.go
@@ -7,8 +7,5 @@ import (
 func BenchmarkIndex(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Index("../../enron_mail_20110402")
-		// database_path := "../../../../" + "enron_mail_20110402" + "/maildir"
-		// _ = listSubFolders(database_path)
-		//index_data()
 	}
 }
